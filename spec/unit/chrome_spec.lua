439cd087f6a967e7ccd99eef4f11d7f7c9ce6fcf
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-03-28 19:33:34 +0530

    test: chrome based bookmarks spec

---
+++ spec/unit/chrome_spec.lua
@@ -0,0 +1,65 @@
+local chrome = require "telescope._extensions.bookmarks.chrome"
+local utils = require "telescope._extensions.bookmarks.utils"
+
+describe("chrome", function()
+  describe("collect_bookmarks", function()
+    local match = require "luassert.match"
+
+    before_each(function()
+      stub(utils, "warn")
+    end)
+
+    after_each(function()
+      utils.warn:revert()
+    end)
+
+    it("should warn if OS not supported", function()
+      local bookmarks = chrome.collect_bookmarks {
+        os_name = "random",
+        selected_browser = "chrome",
+      }
+
+      assert.stub(utils.warn).was_called_with(match.is_string())
+      assert.is_nil(bookmarks)
+    end)
+
+    it("should warn if file is absent", function()
+      local bookmarks = chrome.collect_bookmarks {
+        os_name = "Darwin",
+        selected_browser = "chrome",
+        os_homedir = ".",
+      }
+
+      assert.stub(utils.warn).was_called_with(match.is_string())
+      assert.is_nil(bookmarks)
+    end)
+  end)
+
+  describe("parse_bookmarks_data", function()
+    it("should parse bookmarks file", function()
+      local bookmarks = chrome.collect_bookmarks {
+        os_name = "Darwin",
+        selected_browser = "chrome",
+        os_homedir = "spec/fixtures",
+      }
+
+      assert.are.same(bookmarks, {
+        {
+          name = "Google",
+          path = "search/Google",
+          url = "https://google.com/",
+        },
+        {
+          name = "DuckDuckGo",
+          path = "search/nested/DuckDuckGo",
+          url = "https://duckduckgo.com/",
+        },
+        {
+          name = "GitHub",
+          path = "GitHub",
+          url = "https://github.com/",
+        },
+      })
+    end)
+  end)
+end)
