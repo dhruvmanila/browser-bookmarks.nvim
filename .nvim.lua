e64a73cf327b20993c358cd9499358c4926ec745
Dhruv Manilawala <dhruvmanila@gmail.com>
2023-01-14 15:38:21 +0530

    chore: add .nvim.lua

---
+++ .nvim.lua
@@ -0,0 +1,19 @@
+-- Setup `nvim-test` to use `vusted` for lua instead.
+--
+-- To source this file automatically when Neovim is opened, set the exrc option
+-- in your `init.lua` file:
+--
+--    `vim.opt.exrc = true`
+--
+-- https://github.com/klen/nvim-test
+
+local nvim_test, ok = pcall(require, "nvim-test")
+if not ok then
+  return
+end
+
+nvim_test.setup {
+  runners = {
+    lua = "nvim-test.runners.vusted",
+  },
+}
