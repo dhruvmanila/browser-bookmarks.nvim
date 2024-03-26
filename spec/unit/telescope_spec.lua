0e76e0bf8149a55d404f403b82e3620b51c3e313
Dhruv Manilawala <dhruvmanila@gmail.com>
2024-03-26 08:30:53 +0530

    Update telescope extension test cases

--- spec/unit/telescope_spec.lua
+++ spec/unit/telescope_spec.lua
@@ -1,33 +1,28 @@
+local utils = require "browser_bookmarks.utils"
+
 describe("telescope bookmarks", function()
   insulate("config", function()
     local bookmarks = require "telescope._extensions.bookmarks"
     local config = require "browser_bookmarks.config"
 
-    it("should prefer user config over defaults", function()
-      local test_config = {
-        selected_browser = "firefox",
-        profile_name = "default",
-        config_dir = "/home/user",
-        full_path = false,
-        url_open_command = "xdg-open",
-        url_open_plugin = "vim_external",
-        buku_include_tags = true,
-        debug = true,
-      }
-      assert.are.same(config.values, config._defaults)
-      bookmarks.setup(test_config)
-      assert.are.same(config.values, test_config)
+    before_each(function()
+      stub(utils, "warn")
     end)
-  end)
 
-  insulate("entrypoint", function()
-    local bookmarks = require "telescope._extensions.bookmarks"
+    after_each(function()
+      utils.warn:revert()
+      -- Reset the config
+      config.setup()
+    end)
 
-    it("should error if browser not supported", function()
-      assert.error_matches(function()
-        bookmarks.setup { selected_browser = "random" }
-        bookmarks.exports.bookmarks()
-      end, "Unsupported browser")
+    it("should warn if configuring via telescope extension", function()
+      local test_config = {
+        selected_browser = "random",
+      }
+      local default_browser = config._defaults.selected_browser
+      bookmarks.setup(test_config)
+      assert.stub(utils.warn).was_called(1)
+      assert.are.same(config.values.selected_browser, default_browser)
     end)
   end)
 end)
